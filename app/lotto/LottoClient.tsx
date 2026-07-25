245b2c9a67eb153ccdb89029152162a7742c557e
Minhyeok Kim <kmh1902@gmail.com>
2026-07-26 08:57:20 +0900

    Update lotto result for round 1234

--- app/lotto/LottoClient.tsx
+++ app/lotto/LottoClient.tsx
@@ -67,6 +67,7 @@ const RECENT_RESULTS_PAGE_SIZE = 5;
 
 // 지난 회차 당첨번호
 const RECENT_RESULTS = [
+  { round: 1234, date: "2026-07-25", numbers: [1, 15, 19, 31, 35, 43], bonus: 27 },
   { round: 1233, date: "2026-07-18", numbers: [2, 7, 20, 25, 37, 40], bonus: 29 },
   { round: 1232, date: "2026-07-11", numbers: [12, 15, 19, 22, 24, 36], bonus: 3 },
   { round: 1231, date: "2026-07-04", numbers: [4, 13, 14, 18, 31, 38], bonus: 15 },
